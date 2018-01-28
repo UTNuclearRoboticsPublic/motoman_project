808a320130361f965d6e5f2d6a7df95182162892
MoriKen254 <masamorobo@gmail.com>
2018-01-28 19:55:59 +0900

    [fix] remove unnecessary -

--- .pre_process.sh
+++ .pre_process.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Start installing PCL"
 
-- yes | sudo apt-get install software-properties-common python-software-properties; sudo apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;
+yes | sudo apt-get install software-properties-common python-software-properties; sudo apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;
 
 echo "Finish installing PCL"
 
