1f0728b1447a291c149ae6f202077ee547e73460
MoriKen254 <masamorobo@gmail.com>
2018-01-28 22:15:51 +0900

    [fix] add sudo install to pre_process.

--- .pre_process.sh
+++ .pre_process.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+echo "Start installing sudo"
+apt-get update && apt-get install -y sudo && rm -rf /var/lib/apt/lists/
+echo "Finish installing sudo"
+
 echo "Start installing PCL"
-
 sudo apt-get install -y software-properties-common python-software-properties; sudo apt-add-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;
-
 echo "Finish installing PCL"
