197697f7014a85a1e32742612d1bb2c770a27ab6
MoriKen254 <masamorobo@gmail.com>
2018-01-28 23:07:34 +0900

    [fix] delete launch test.

--- .pre_process.sh
+++ .pre_process.sh
@@ -5,5 +5,5 @@ apt-get update && apt-get install -y sudo && rm -rf /var/lib/apt/lists/
 echo "***** Finish installing sudo *****"
 
 echo "***** Start installing PCL *****"
-sudo apt-get update -qq; sudo apt-get install -y software-properties-common python-software-properties; sudo apt-add-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;
+sudo apt-get update -qq; sudo apt-get install -y software-properties-common python-software-properties; sudo apt-add-repository -y ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-dev;
 echo "***** Finish installing PCL *****"
